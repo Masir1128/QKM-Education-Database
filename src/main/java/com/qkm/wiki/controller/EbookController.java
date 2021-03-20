52c5ed99a2a8ac91a0b4ec5d75993baa936475b9
“masir <1205211289@qq.com>
2021-03-20 20:17:14 +0800

    3-8 制作CopyUtil 封装BesnUtils

--- src/main/java/com/qkm/wiki/controller/EbookController.java
+++ src/main/java/com/qkm/wiki/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.qkm.wiki.controller;
 
 import com.qkm.wiki.domain.Ebook;
+import com.qkm.wiki.req.EbookReq;
 import com.qkm.wiki.resp.CommonResp;
+import com.qkm.wiki.resp.EbookResp;
 import com.qkm.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,9 +26,9 @@ public class EbookController {
     private EbookService EbookService;
 
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = EbookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = EbookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/qkm/wiki/service/EbookService.java
+++ src/main/java/com/qkm/wiki/service/EbookService.java
@@ -1,10 +1,17 @@
 package com.qkm.wiki.service;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.qkm.wiki.domain.Ebook;
+import com.qkm.wiki.domain.EbookExample;
 import com.qkm.wiki.mapper.EbookMapper;
+import com.qkm.wiki.req.EbookReq;
+import com.qkm.wiki.resp.EbookResp;
+import com.qkm.wiki.util.CopyUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,7 +24,23 @@ public class EbookService {
     @Resource
     private EbookMapper EbookMapper;
 
-    public List<Ebook> list(){
-        return EbookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = EbookMapper.selectByExample(ebookExample);
+
+//        List<EbookResp> respList = new ArrayList<>();
+//        for(Ebook ebook : ebookList){
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            // 对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
+
     }
 }

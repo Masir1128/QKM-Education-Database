3e12c2c4a60b518974c068750a001a08407a41e6
“masir <1205211289@qq.com>
2021-03-20 15:11:30 +0800

    2-7 增加测试接口 http

--- src/main/java/com/qkm/wiki/controller/TestController.java
+++ src/main/java/com/qkm/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.qkm.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "李群";
+    }
 
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World ! Post." + name;
     }
 }

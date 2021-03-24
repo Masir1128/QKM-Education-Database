e8ed7ff2c1b3fc480e98349f18006a6ed61c6ebd
“masir <1205211289@qq.com>
2021-03-24 10:55:02 +0800

    4-16 电子书管理分类功能，增加树形结构

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -37,6 +37,10 @@
           <img v-if="cover" :src="cover" alt="avatar" />
         </template>
 
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
+
         <template v-slot:action="{ text, record }">
           <a-space size="small">
             <a-button type="primary" @click="edit(record)">
@@ -72,11 +76,12 @@
       <a-form-item label="名称">
         <a-input v-model:value="ebook.name" />
       </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category1" />
-      </a-form-item>
-      <a-form-item label="分类二">
-        <a-input v-model:value="ebook.category2" />
+      <a-form-item label="分类">
+        <a-cascader
+          v-model:value="categoryIds"
+          :field-names="{label:'name',value:'id',children:'children'}"
+          :options="level1"
+        />
       </a-form-item>
       <a-form-item label="描述">
         <a-input v-model:value="ebook.description" type="textarea" />
@@ -112,42 +117,39 @@ export default defineComponent({
       },
       {
         title: '名称',
-        dataIndex: 'name',
+        dataIndex: 'name'
       },
       {
-        title: '分类一',
-        key: 'category1_id',
-        dataIndex: 'category1_id'
-      },
-      {
-        title: '分类二',
-        key: 'category2_id',
-        dataIndex: 'category2_id'
+        title: '分类',
+        slots: { customRender: 'category' }
       },
       {
         title: '文档数',
-        dataIndex: 'doc_count'
+        dataIndex: 'docCount'
       },
       {
         title: '阅读数',
-        dataIndex: 'view_count'
+        dataIndex: 'viewCount'
       },
       {
         title: '点赞数',
-        dataIndex: 'vote_count'
+        dataIndex: 'voteCount'
       },
       {
-        title: '操作',
+        title: 'Action',
         key: 'action',
-        slots: {customRender: 'action'}
+        slots: { customRender: 'action' }
       }
     ];
+
 
     /**
      * 数据查询
      */
     const handleQuery = (params: any) => {
       loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      ebooks.value = [];
       axios.get("http://localhost:8880/ebook/list", {
         params:{
           page:params.page,
@@ -181,11 +183,17 @@ export default defineComponent({
       });
     };
     // --------------- 表单 ------------
-    const ebook = ref({});
+    /**
+     * 数组 [100,10] 对应 机器人/PA
+     */
+    const categoryIds =ref();
+    const ebook = ref();
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
       //===================
       axios.post("http://localhost:8880/ebook/save", ebook.value).then((response) =>{
         modalVisible.value = false;
@@ -208,6 +216,7 @@ export default defineComponent({
     const edit =(record: any) =>{
       modalVisible.value = true;
       ebook.value = Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id,ebook.value.category2Id]
     };
 
     /**
@@ -235,10 +244,49 @@ export default defineComponent({
       });
     };
 
+    const level1 = ref();
+    let categorys:any;
+    /**
+     * 查询所有分类
+     */
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("http://localhost:8880/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+
+          // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const getCategoryName = (cid: number) => {
+      // console.log(cid)
+      let result = "";
+      categorys.forEach((item: any) => {
+        if (item.id === cid) {
+          // return item.name; // 注意，这里直接return不起作用
+          result = item.name;
+        }
+      });
+      return result;
+    };
+
     onMounted(() =>{
+      handleQueryCategory();
       handleQuery({
         page:1,
-        size:pagination.value.pageSize
+        size:pagination.value.pageSize,
       });
     });
     return{
@@ -249,11 +297,14 @@ export default defineComponent({
       loading,
       handleTableChange,
       handleQuery,
+      getCategoryName,
 
       edit,
       ebook,
       add,
       handleDelete,
+      categoryIds,
+      level1,
 
 
       modalVisible,

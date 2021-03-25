0d1a5986425aeee7aabb016512790a1828a7d399
“masir <1205211289@qq.com>
2021-03-25 15:58:56 +0800

    5-10 集成富文本框插件wangEditor@4.6.3,在编辑文档页面进行显示

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -82,6 +82,10 @@
         <a-input v-model:value="doc.sort" />
       </a-form-item>
 
+      <a-form-item label="内容">
+        <a-input v-model:value="doc.sort" />
+      </a-form-item>
+        <div id="content"></div>
     </a-form>
   </a-modal>
 </template>
@@ -92,6 +96,7 @@ import axios from 'axios';
 import { message } from 'ant-design-vue';
 import { Tool } from '@/util/tool';
 import {useRoute} from "vue-router";
+import E from 'wangeditor'
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -175,6 +180,10 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    // 定义编辑器
+    const editor = new E('#content')
+
+
     const handleModalOk = () => {
 
       modalLoading.value = true;
@@ -273,12 +282,16 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
     };
 
     /**
      * 新增
      */
     const add =() =>{
+
       modalVisible.value = true;
       doc.value = {
         ebookId:route.query.ebookId,
@@ -288,6 +301,10 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
+
     };
 
     /**
@@ -309,6 +326,7 @@ export default defineComponent({
 
     onMounted(() =>{
       handleQuery();
+
     });
     return{
       param,

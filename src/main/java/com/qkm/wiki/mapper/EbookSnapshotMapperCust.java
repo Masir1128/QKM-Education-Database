6ed4b7f8ac6e2be211111e63586b26648af26aaf
“masir <1205211289@qq.com>
2021-03-31 17:45:40 +0800

    6-9 新增电子书快照功能，编写数据库脚本

---
+++ src/main/java/com/qkm/wiki/mapper/EbookSnapshotMapperCust.java
@@ -0,0 +1,9 @@
+package com.qkm.wiki.mapper;
+
+
+public interface EbookSnapshotMapperCust {
+
+    public void genSnapshot();
+
+
+}
---
+++ src/main/java/com/qkm/wiki/service/EbookSnapshotService.java
@@ -0,0 +1,21 @@
+package com.qkm.wiki.service;
+
+import com.qkm.wiki.mapper.EbookSnapshotMapperCust;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookSnapshotService {
+
+    @Resource
+    private EbookSnapshotMapperCust ebookSnapshotMapperCust;
+
+    public void genSnapshot() {
+        ebookSnapshotMapperCust.genSnapshot();
+    }
+
+
+
+}
